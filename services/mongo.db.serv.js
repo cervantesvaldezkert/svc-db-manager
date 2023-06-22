5d0f1ed282d06476360d8831b9d8c0aac2110d2a
cervantesvaldezkert <cervantesvaldezkert@gmail.com>
2023-06-22 15:52:55 +0800

    update mongodb serv
    add initialize connection function for mongodb

--- services/mongo.db.serv.js
+++ services/mongo.db.serv.js
@@ -3,19 +3,16 @@ const mongoDBConfig = require('../cofigs/tasks.mongodb.config');
 const uri = mongoDBConfig.connectionString;
 const client = new MongoClient(uri);
 
-//dynamic method for inserting document in mongoDB
+//method for inserting document in mongoDB
 //supply $data as array of objects
 //supply specific dbName and collection 
 exports.insert = async (data, dbName, collectionName) => {
-
   try {
-      await client.connect();
-      const database = client.db(dbName);
-      const collection = database.collection(collectionName);
+      const collection = await initConnection(dbName, collectionName);
 
       const insertManyResult = await collection.insertMany(data);
-      console.log(`${insertManyResult.insertedCount} documents successfully inserted.\n`);
 
+      await client.close();
       return insertManyResult;
     } catch (err) {
       console.error(`Something went wrong trying to insert the new documents: ${err}\n`);
@@ -23,87 +20,91 @@ exports.insert = async (data, dbName, collectionName) => {
 
 }
 
+//method for finding document by id in mongoDB
+//supply docId
+//supply specific dbName and collection 
 exports.findByDocId = async (docId, dbName, collectionName) => {
-
-  await client.connect();
-  const database = client.db(dbName);
-  const collection = database.collection(collectionName);
-  console.log(docId);
-
+  try {
+  const collection = await initConnection(dbName, collectionName);
   const findQuery = { docId: { $eq: docId } };
-
   let result = [];
 
-  try {
     const cursor = await collection.find(findQuery).sort({ logDate: 1 });
     await cursor.forEach(recipe => {
       result.push(recipe);
     });
     
+    await client.close();
     return result;
   } catch (err) {
     console.error(`Something went wrong trying to find the documents: ${err}\n`);
   }
 }
 
+//method for fetching all document without limit in mongoDB
+//supply specific dbName and collection 
 exports.findAll = async (dbName, collectionName) => {
-
-  await client.connect();
-  const database = client.db(dbName);
-  const collection = database.collection(collectionName);
-
-  let result = [];
-
   try {
+    const collection = await initConnection(dbName, collectionName);
+    let result = [];
+
     const cursor = await collection.find().sort({ logDate: 1 });
     await cursor.forEach(data => {
       result.push(data);
     });
 
+    await client.close();
     return result;
   } catch (err) {
     console.error(`Something went wrong trying to find the documents: ${err}\n`);
   }
 }
 
+//method for updating document without limit in mongoDB
+//supply specific dbName and collection 
 exports.update = async (req, dbName, collectionName) => {
-
-  await client.connect();
-  const database = client.db(dbName);
-  const collection = database.collection(collectionName);
-
-  const findOneQuery = { docId: req.docId };
-  const updateDoc = { $set: req.data };
-  const updateOptions = { returnOriginal: false };
-
   try {
+    const collection = await initConnection(dbName, collectionName);
+    const findOneQuery = { docId: req.docId };
+    const updateDoc = { $set: req.data };
+    const updateOptions = { returnOriginal: false };
+
     const updateResult = await collection.findOneAndUpdate(
       findOneQuery,
       updateDoc,
       updateOptions,
     );
 
+    await client.close();
     return updateResult
-    
   } catch (err) {
     console.error(`Something went wrong trying to update one document: ${err}\n`);
   }
 }
 
+//method for deleting document by DocId in mongoDB
+//supply docId
+//supply specific dbName and collection 
 exports.deleteByDocId = async (docId, dbName, collectionName) => {
-
-  await client.connect();
-  const database = client.db(dbName);
-  const collection = database.collection(collectionName);
-  const deleteQuery = { docId: { $eq: docId } };
-
   try {
+    const collection = initConnection(dbName, collectionName);
+    const deleteQuery = { docId: { $eq: docId } };
+
     const deleteResult = await collection.deleteMany(deleteQuery);
-    console.log(`Deleted ${deleteResult.deletedCount} documents\n`);
+
+    await client.close();
     return deleteResult;
   } catch (err) {
     console.error(`Something went wrong trying to delete documents: ${err}\n`);
   }
   
+}
 
+//generic function to initialize connection to mongodb
+initConnection = async(dbName, collectionName) => {
+  await client.connect();
+  const database = client.db(dbName);
+  const collection = database.collection(collectionName);
+
+  return collection;
 }
